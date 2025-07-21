f1f8c915eb9ea3ae192fe440a1ee5da10cd65297
Xavier Huix Trenco <xavierhuixtrenco@MacBook-Pro-de-Xavier.local>
2025-07-21 07:51:02 +0200

    continue

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,20 +1,22 @@
 import '../polyfills/structuredClone';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import { createClient, processLock } from '@supabase/supabase-js';
 import { AppState } from 'react-native';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-	auth: {
-		storage: AsyncStorage,
-		autoRefreshToken: true,
-		persistSession: true,
-		detectSessionInUrl: false,
+export const supabase = createClient(
+	process.env.EXPO_PUBLIC_SUPABASE_URL!,
+	process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+	{
+		auth: {
+			storage: AsyncStorage,
+			autoRefreshToken: true,
+			persistSession: true,
+			detectSessionInUrl: false,
+			lock: processLock,
+		},
 	},
-});
+);
 
 // Auto-refresh session when app becomes active
 AppState.addEventListener('change', (state) => {
